dfc2b8ce4de8a2323105912bd1ffc273e3d089f7
ariel151515 <ndcdigital2020@gmail.com>
2024-07-03 17:35:39 -0300

    screen Loading terminada

--- AireService/src/screen/Loading.jsx
+++ AireService/src/screen/Loading.jsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ActivityIndicator, TouchableOpacity } from 'react-native'
 import React from 'react'
 
+// Imagenes
+import logo from './../../assets/logo.png';
+
 const Loading = () => {
+
+  const handlePress = () => {
+    // Aquí defines la función que quieres ejecutar
+    console.log('Imagen tocada');
+  };
+
+  
   return (
-    <View>
-      <Text>Loading.................</Text>
-    </View>
+    <View className="flex-1 justify-center">
+        <View className="items-center gap-20">
+          <TouchableOpacity onPress={handlePress}>
+            <Image source={logo}/>
+          </TouchableOpacity>
+            <ActivityIndicator size="large" color="#07C7FB" />
+        </View>
+      </View>
   )
 }
 
